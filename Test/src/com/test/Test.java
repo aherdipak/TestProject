69a60f7bd9578a1ebeed53772ac03289c64be73a
dipakaher <dipakaher@192.168.1.101>
2018-11-17 00:52:25 +0530

    new project intialisation

---
+++ Test/src/com/test/Test.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("I am from Macbook Air");
+		System.out.println("This is my first program for git");
+		
+		System.out.println("test2");
+		
+		
+		
+	}
+}
